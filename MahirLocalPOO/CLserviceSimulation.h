e5f282819f7de2f86430035576a53e1161fbcd37
mahircesi <mahir.naouzhomburger@viacesi.fr>
2023-12-10 14:32:23 +0100

    Marge commerciale OK

---
+++ MahirLocalPOO/CLserviceSimulation.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "CLcad.h"
+#include "CLmapSimulation.h"
+namespace NS_Svc_Simu {
+	ref class CLserviceSimulation
+	{
+	private:
+		NS_Comp_Data::CLcad^ oCad;
+		NS_Comp_MappageSimu::CLmapSimulation^ oMapSimu;
+	public:
+		CLserviceSimulation(void);
+		System::Data::DataSet^ MargeCommerciale5(System::String^ dataTableName, int m);
+	};
+}
